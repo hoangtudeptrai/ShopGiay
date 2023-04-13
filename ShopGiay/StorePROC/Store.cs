391ec77a4758eaf2cd330c7aa21f301d0cbe0328
hoangtudeptrai <92379642+hoangtudeptrai@users.noreply.github.com>
2023-04-13 20:37:04 +0700

    123

--- ShopGiay/StorePROC/Store.cs
+++ ShopGiay/StorePROC/Store.cs
@@ -1,4 +1,8 @@
-﻿using static Azure.Core.HttpHeader;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Policy;
+using static Azure.Core.HttpHeader;
 
 namespace ShopGiay.StorePROC
 {
@@ -53,48 +57,149 @@ namespace ShopGiay.StorePROC
         #endregion
 
         #region XoaTaiKhoan
-//        ALTER PROC TuDH_XoaTaiKhoan
-//            @ID_TaiKhoan INT = 0
+        //        ALTER PROC TuDH_XoaTaiKhoan
+        //            @ID_TaiKhoan INT = 0
+        //AS
+        //BEGIN
+
+
+
+        //            BEGIN TRAN
+
+        //        BEGIN TRY
+
+        //        DECLARE @ID_TaiKhoanSQL INT =  CASE
+        //                                            WHEN EXISTS(SELECT 1 FROM dbo.TaiKhoan t WHERE ID_TaiKhoan = @ID_TaiKhoan)
+
+        //                                            THEN(SELECT TOP 1 t.ID_TaiKhoan FROM dbo.TaiKhoan t WHERE ID_TaiKhoan = @ID_TaiKhoan)
+
+        //                                                        ELSE -1 END;
+
+        //			IF @ID_TaiKhoanSQL > 0
+
+        //                BEGIN
+        //                DELETE TaiKhoan
+        //            WHERE ID_TaiKhoan = @ID_TaiKhoan
+        //            SELECT 1 AS re
+
+        //                END
+        //            ELSE
+
+        //                BEGIN
+
+        //                    SELECT -1 AS re
+
+        //                END
+        //        COMMIT TRAN
+        //    END TRYx
+        //    BEGIN CATCH
+
+        //        SELECT -1 AS re;
+        //        ROLLBACK TRAN
+
+        //    END CATCH
+        //END
+        #endregion
+
+        #region Them moi san pham
+        //        ALTER PROC[dbo].[TuDH_ThemMoiSanPham]
+        //        @TenSanPham NVARCHAR(MAX) ='',
+        //	@GioiThieuSanPham NVARCHAR(MAX) = '',
+        //	@GiaSanPham FLOAT = 0,
+        //    @PhiVanChuyen FLOAT = 0
+        //AS
+        //BEGIN
+
+
+
+        //    BEGIN TRAN
+
+        //        BEGIN TRY
+        //            DECLARE @table TABLE(ID INT);
+        //        DECLARE @re INT = 0;
+
+        //			INSERT INTO
+
+        //            DM_SanPham(TenSanPham, GiaSanPham, PhiVanChuyen, GioiThieuSanPham)
+
+        //            OUTPUT inserted.ID_SanPham INTO @TABLE
+
+        //            VALUES(@TenSanPham, @GiaSanPham, @PhiVanChuyen, @GioiThieuSanPham)
+
+        //            SET @re = (SELECT TOP 1 ID FROM @TABLE )
+
+
+        //            SELECT @re AS RE
+
+
+
+        //        COMMIT TRAN
+
+        //    END TRY
+
+        //    BEGIN CATCH
+
+        //        SELECT -1 AS re;
+        //        ROLLBACK TRAN
+
+        //    END CATCH
+        //END
+
+        #endregion
+
+        #region Them chi tiet san pham
+        //        ALTER PROC[dbo].[TUDH_Insert_DM_ChiTietSanPham]
+        //        @ID_SanPham INT = 0,
+        //    @ID_MauSac INT = 0,
+        //	@SoLuong INT = 0,
+        //    @Size INT = 0
+        //AS
+        //BEGIN
+
+        //    BEGIN TRAN
+
+        //        BEGIN TRY
+        //            DECLARE @table TABLE(ID INT);
+        //        DECLARE @re INT = 0;
+
+        //			INSERT INTO
+
+        //            DM_ChiTietSanPham(ID_SanPham, ID_MauSac, Size, SoLuong)
+
+        //            OUTPUT inserted.ID_SanPham INTO @TABLE
+
+        //            VALUES(@ID_SanPham, @ID_MauSac, @Size, @SoLuong)
+        //            SET @re = (SELECT TOP 1 ID FROM @TABLE )
+
+
+        //            SELECT @re AS RE
+
+
+
+        //        COMMIT TRAN
+
+        //    END TRY
+
+        //    BEGIN CATCH
+
+        //        SELECT -1 AS re;
+        //        ROLLBACK TRAN
+
+        //    END CATCH
+        //END
+        #endregion
+
+        #region Them anh
+//        ALTER PROC[dbo].[TuDH_ThemAnh]
+
+//        @ID_SanPham int = 0,
+//	@URL NVARCHAR(MAX) = ''
 //AS
 //BEGIN
 
-
-
-//            BEGIN TRAN
-
-//        BEGIN TRY
-
-//        DECLARE @ID_TaiKhoanSQL INT =  CASE
-//                                            WHEN EXISTS(SELECT 1 FROM dbo.TaiKhoan t WHERE ID_TaiKhoan = @ID_TaiKhoan)
-
-//                                            THEN(SELECT TOP 1 t.ID_TaiKhoan FROM dbo.TaiKhoan t WHERE ID_TaiKhoan = @ID_TaiKhoan)
-
-//                                                        ELSE -1 END;
-
-//			IF @ID_TaiKhoanSQL > 0
-
-//                BEGIN
-//                DELETE TaiKhoan
-//            WHERE ID_TaiKhoan = @ID_TaiKhoan
-//            SELECT 1 AS re
-
-//                END
-//            ELSE
-
-//                BEGIN
-
-//                    SELECT -1 AS re
-
-//                END
-//        COMMIT TRAN
-//    END TRYx
-//    BEGIN CATCH
-
-//        SELECT -1 AS re;
-//        ROLLBACK TRAN
-
-//    END CATCH
+//    INSERT INTO DM_Image(ID_SanPham, URL) VALUES(@ID_SanPham, @URL)
 //END
         #endregion
+
     }
 }
